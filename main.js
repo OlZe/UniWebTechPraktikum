4ddbb4de8be83c7f151d05fe87fa74bbea4abef4
OlZe <oliverzeidler97@gmail.com>
2020-12-22 12:16:45 +0100

    p8a3

--- main.js
+++ main.js
@@ -12,9 +12,14 @@ function Raum(nr, bez, gebäude, kapazität, austattung) {
     this.gebäude = gebäude;
     this.kapazität = kapazität;
     this.austattung = austattung;
+    this.buchungen = [];
+    this.addBuchung = (buchung) => {
+        this.buchungen.push(buchung);
+        this.buchungen.sort((a, b) => b.startZeit - a.startZeit);
+    };
 }
 
-let myRoom = new Raum(69, 'Ein Raum', 'Ein Gebäude', 30, ['1 Beamer', '2 Tafeln']);
+
 
 let buchung = {
     bezeichnung: 'Blinken',
@@ -24,3 +29,24 @@ let buchung = {
     anzahlTeilnehmer: 1,
     beschreibung: 'Augen anfeuchten'
 };
+let buchung2 = {
+    bezeichnung: 'Blinken',
+    startZeit: new Date(2020, 11, 23, 11, 03, 34, 579),
+    endZeit: new Date(2020, 11, 23, 11, 03, 34, 979),
+    gebuchtVon: 'ich',
+    anzahlTeilnehmer: 1,
+    beschreibung: 'Augen anfeuchten'
+};
+let buchung3 = {
+    bezeichnung: 'Blinken',
+    startZeit: new Date(2021, 11, 22, 11, 03, 34, 579),
+    endZeit: new Date(2021, 11, 22, 11, 03, 34, 979),
+    gebuchtVon: 'ich',
+    anzahlTeilnehmer: 1,
+    beschreibung: 'Augen anfeuchten'
+};
+
+let myRoom = new Raum(69, 'Ein Raum', 'Ein Gebäude', 30, ['1 Beamer', '2 Tafeln']);
+myRoom.addBuchung(buchung);
+myRoom.addBuchung(buchung2);
+myRoom.addBuchung(buchung3);
