d6bd544aa15f58d56c1608038c6ff0564659cb9f
OlZe <oliverzeidler97@gmail.com>
2020-12-22 11:00:55 +0100

    p8a1

--- main.js
+++ main.js
@@ -3,3 +3,15 @@ function getViewportWidth() {
 }
 
 console.log(`Die Viewport-Breite beträgt: ${getViewportWidth()} Pixel.`);
+
+
+
+function Raum(nr, bez, gebäude, kapazität, austattung) {
+    this.nummer = nr;
+    this.bezeichnung = bez;
+    this.gebäude = gebäude;
+    this.kapazität = kapazität;
+    this.austattung = austattung;
+}
+
+let myRoom = new Raum(69, 'Ein Raum', 'Ein Gebäude', 30, ['1 Beamer', '2 Tafeln']);
